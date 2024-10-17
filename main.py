5577b84d785400cfe23bc0f33cb0c85098ff9d98
Mateusz Urbańczyk <mateusz.urbanczyk11@gmail.com>
2024-10-17 13:19:55 +0200

    Empty endpoints created

--- main.py
+++ main.py
@@ -1,16 +1,89 @@
 import uvicorn
 from fastapi import FastAPI
-from schemas import Login
+from schemas import Email, Login, CategoryName, CategoryAdd, CategoryUpdate, ProductName, \
+    ProductAdd, ProductUpdate, NoteName, NoteAdd, NoteUpdate
 
 app = FastAPI()
 
+@app.post('/refresh-token')
+async def refresh_token(refresh_token: str):
+    print("token refreshed")
+
+@app.delete('/account')
+async def delete_account(access_token: str):
+    print("account deleted")
 
 @app.post('/login')
 async def login(data: Login):
     print("logged")
 
+@app.post('/register')
+async def register(data: Login):
+    print("registered")
 
+@app.post('/refresh-password')
+async def refresh_password(data: Email):
+    print("password refreshed")
 
+@app.post('/logout')
+async def logout(access_token: str):
+    print("logged out")
+
+@app.get('/category/all')
+async def get_categories(access_token: str):
+    print("all categories")
+
+@app.post('/category')
+async def add_category(access_token: str, data: CategoryAdd):
+    print("category added")
+
+@app.put('/category')
+async def edit_category(access_token: str, data: CategoryUpdate):
+    print("category edited")
+
+@app.delete('/category')
+async def delete_category(access_token: str, data: CategoryName):
+    print("category deleted")
+
+@app.get('/category-products')
+async def get_category_products(access_token: str, data: CategoryName):
+    print("all category_products")
+
+@app.get('/product')
+async def get_product(access_token: str, data: ProductName):
+    print("product fetched")
+
+@app.post('/product')
+async def add_product(access_token: str, data: ProductAdd):
+    print("product added")
+
+@app.put('/product')
+async def edit_product(access_token: str, data: ProductUpdate):
+    print("product edited")
+
+@app.delete('/product')
+async def delete_product(access_token: str, data: ProductName):
+    print("product deleted")
+
+@app.get('/note/all')
+async def get_notes(access_token: str):
+    print("all notes")
+
+@app.get('/note')
+async def get_note(access_token: str, data: NoteName):
+    print("note")
+
+@app.post('/note')
+async def add_note(access_token: str, data: NoteAdd):
+    print("note added")
+
+@app.put('/note')
+async def edit_note(access_token: str, data: NoteUpdate):
+    print("note edited")
+
+@app.delete('/note')
+async def delete_note(access_token: str, data: NoteName):
+    print("note deleted")
 
 
 if __name__ == '__main__':
--- schemas.py
+++ schemas.py
@@ -1,10 +1,102 @@
 from pydantic import BaseModel
 
 
-class Login(BaseModel):
+class Email(BaseModel):
     email: str
+
+    class Config:
+        from_attributes = True
+
+
+class Login(Email):
     password: str
 
 
     class Config:
         from_attributes = True
+
+
+class CategoryName(BaseModel):
+    name: str
+
+
+    class Config:
+        from_attributes = True
+
+
+class CategoryAdd(CategoryName):
+    photo: str | None = None
+
+
+    class Config:
+        from_attributes = True
+
+
+class CategoryUpdate(BaseModel):
+    old_name: str
+    name: str | None = None
+    photo: str | None = None
+
+
+    class Config:
+        from_attributes = True
+
+
+class ProductName(BaseModel):
+    name: str 
+
+
+    class Config:
+        from_attributes = True
+    
+
+class ProductAdd(ProductName):
+    quantity: str
+    photo: str | None = None
+    audio: str | None = None
+    video: str | None = None
+
+
+    class Config:
+        from_attributes = True
+
+
+class ProductUpdate(BaseModel):
+    old_name: str
+    quantity: str | None = None
+    photo: str | None = None 
+    audio: str | None = None
+    video: str | None = None
+
+
+    class Config:
+        from_attributes = True
+
+
+class NoteName(BaseModel):
+    name: str
+
+
+    class Config:
+        from_attributes = True
+
+
+class NoteAdd(NoteName):
+    text: str
+
+
+    class Config:
+        from_attributes = True
+
+
+class NoteUpdate(BaseModel):
+    old_name: str
+    name: str | None = None
+    text: str
+
+
+    class Config:
+        from_attributes = True
+
+
+
