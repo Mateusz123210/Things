b769148a1741cac91aa084ba0d5eeca9e1c01119
MatKomp <matkomp@MatKomps-Mac.local>
2024-11-04 22:12:34 +0100

    Empty project created

---
+++ Things/Things/ThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThingsApp.swift
+//  Things
+//
+//  Created by MatKomp on 04/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Things/ThingsTests/ThingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThingsTests.swift
+//  ThingsTests
+//
+//  Created by MatKomp on 04/11/2024.
+//
+
+import Testing
+@testable import Things
+
+struct ThingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
