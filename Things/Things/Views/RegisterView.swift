29e789a948b2bf5c67084dd1c1ee1b039d323cf3
Mateusz123210 <mateusz.urbanczyk11@gmail.com>
2024-11-07 21:04:00 +0100

    Better register view interface lookup

--- Things/Things/Views/RegisterView.swift
+++ Things/Things/Views/RegisterView.swift
@@ -22,47 +22,47 @@ struct RegisterView: View{
     }
     
     var body: some View{
-        //GeometryReader{ geometry in
+        GeometryReader{ geometry in
             VStack{
-                //let width90 = geometry.size.width * 0.9
-                LogoImageView()
+                let height = geometry.size.height
                 VStack{
-                    Text("Register")
-                        .fontWeight(.semibold)
-                        .font(Font.system(size: 32))
-                    Text("Email")
-                        .fontWeight(.light)
-                        .font(Font.system(size: 28))
-                        .foregroundStyle(.lightBlack202C37)
-                    TextField("", text: $email)
-                    //.frame(width: width90)
-                        .background(Color.green)
-                    Text("Password")
-                        .fontWeight(.light)
-                        .font(Font.system(size: 28))
-                        .foregroundStyle(.lightBlack202C37)
-                    TextField("", text: $password)
-                    //.frame(width:width90)
-                        .background(Color.green)
-                }
-                .alert(isPresented: $showAlert){
-                    Alert(title: Text("Important message"), message: Text(alertMessage))
-                }
+                LogoImageView()
+                
+                Text("Register")
+                    .fontWeight(.semibold)
+                    .font(Font.system(size: 32))
+                Text("Email")
+                    .fontWeight(.light)
+                    .font(Font.system(size: 28))
+                    .foregroundStyle(.lightBlack202C37)
+                TextField("", text: $email)
+                    .background(.white)
+                Text("Password")
+                    .fontWeight(.light)
+                    .font(Font.system(size: 28))
+                    .foregroundStyle(.lightBlack202C37)
+                TextField("", text: $password)
+                    .background(.white)
+                
+                    .alert(isPresented: $showAlert){
+                        Alert(title: Text("Important message"), message: Text(alertMessage))
+                    }
                 
                 Button("Register"){
                     //registerUser()
-                }
-                    .fontWeight(.bold)
+                }   .fontWeight(.bold)
                     .font(Font.system(size: 32))
                     .foregroundStyle(.black)
                     .background(.lightBlueD6F1FF)
-                
+                    .buttonStyle(.borderedProminent)
                 
                 Text("Already have an account? Login")
                     .fontWeight(.bold)
                     .font(Font.system(size: 17))
                     .foregroundStyle(.lightBlue00A7FF)
-            }.padding(16)
-        //}
+            }   .padding(16)
+                .background(.backgroundF7F7F7)
+            }  
+        }
     }
 }
